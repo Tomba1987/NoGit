4acc2a255558fccb17666d20b797055e68dd53f0
Tomislav Tomić <milehaker1987@gmail.comcd>
2022-08-18 12:03:05 +0200

    first commit

---
+++ ProbaGit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProbaGit
+//
+//  Created by Tomislav Tomic on 18.08.2022..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("HELLO!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProbaGit/ProbaGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProbaGitApp.swift
+//  ProbaGit
+//
+//  Created by Tomislav Tomic on 18.08.2022..
+//
+
+import SwiftUI
+
+@main
+struct ProbaGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
